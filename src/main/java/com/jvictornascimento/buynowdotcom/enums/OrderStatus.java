7c7226e2f873f0d38551755e223f9fd00a5f763d
jvictornascimento <victojoao789@gmail.com>
2025-06-02 13:54:44 -0300

    feet: Implementing the user erd

---
+++ src/main/java/com/jvictornascimento/buynowdotcom/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.jvictornascimento.buynowdotcom.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ src/main/java/com/jvictornascimento/buynowdotcom/model/Cart.java
@@ -0,0 +1,24 @@
+package com.jvictornascimento.buynowdotcom.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal totalAmount;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
