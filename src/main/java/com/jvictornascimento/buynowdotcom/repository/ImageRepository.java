a7fc53411c4b22be80ee791a5a5cd87d097f38ee
jvictornascimento <victojoao789@gmail.com>
2025-06-02 18:45:35 -0300

    feet: The product image service implementing

---
+++ src/main/java/com/jvictornascimento/buynowdotcom/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.jvictornascimento.buynowdotcom.repository;
+
+import com.jvictornascimento.buynowdotcom.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
---
+++ src/main/java/com/jvictornascimento/buynowdotcom/service/image/ImageService.java
@@ -0,0 +1,83 @@
+package com.jvictornascimento.buynowdotcom.service.image;
+
+import com.jvictornascimento.buynowdotcom.dtos.ImageDto;
+import com.jvictornascimento.buynowdotcom.model.Image;
+import com.jvictornascimento.buynowdotcom.model.Product;
+import com.jvictornascimento.buynowdotcom.repository.ImageRepository;
+import com.jvictornascimento.buynowdotcom.service.product.IProductService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService implements IImageService {
+    private final ImageRepository imageRepository;
+    private final IProductService productService;
+
+    @Override
+    public Image getImageById(Long imageId) {
+        return imageRepository.findById(imageId)
+                .orElseThrow(() -> new EntityNotFoundException("Image not found!"));
+    }
+
+    @Override
+    public void deleteImageById(Long imageId) {
+        imageRepository.findById(imageId).ifPresentOrElse(
+                imageRepository::delete, () -> {
+                    throw new EntityNotFoundException("Image not found!");
+                });
+    }
+
+    @Override
+    public void updateImage(MultipartFile file, Long iamgeId) {
+        Image image = getImageById(iamgeId);
+        try {
+            image.setFileName(file.getOriginalFilename());
+            image.setFileType(file.getContentType());
+            image.setImage(new SerialBlob(file.getBytes()));
+            imageRepository.save(image);
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<ImageDto> saveImages(Long productId, List<MultipartFile> files) {
+        Product product = productService.getProductByID(productId);
+
+        List<ImageDto> savedImagens = new ArrayList<>();
+        for (MultipartFile file : files) {
+            try {
+                Image image = new Image();
+                image.setFileName(file.getOriginalFilename());
+                image.setFileType(file.getContentType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                image.setProduct(product);
+
+                String buildDownloadUrl = "/api/v1/images/image/download";
+                String downoadUrl = buildDownloadUrl + image.getId();
+                image.setDownloadUrl(downoadUrl);
+                Image savedImage = imageRepository.save(image);
+                savedImage.setDownloadUrl(buildDownloadUrl + savedImage.getImage());
+                imageRepository.save(savedImage);
+
+                ImageDto imageDto = new ImageDto();
+                imageDto.setId(savedImage.getId());
+                imageDto.setFileName(savedImage.getFileName());
+                imageDto.setDownloadUrl(savedImage.getDownloadUrl());
+                savedImagens.add(imageDto);
+            } catch (IOException | SQLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return savedImagens;
+    }
+}
