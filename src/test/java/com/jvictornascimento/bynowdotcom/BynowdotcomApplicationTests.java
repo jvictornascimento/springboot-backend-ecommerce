e209a87dae8f3db18101fb140c672145198b7351
jvictornascimento <victojoao789@gmail.com>
2025-06-02 00:35:59 -0300

    feet: Created project

---
+++ src/test/java/com/jvictornascimento/bynowdotcom/BynowdotcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvictornascimento.bynowdotcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BynowdotcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
