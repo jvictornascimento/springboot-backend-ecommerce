6468a2f70a63d04e23c1c3be628812c0f369fa16
jvictornascimento <victojoao789@gmail.com>
2025-06-02 00:35:59 -0300

    feat: Created project

---
+++ src/main/java/com/jvictornascimento/bynowdotcom/BynowdotcomApplication.java
@@ -0,0 +1,13 @@
+package com.jvictornascimento.bynowdotcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BynowdotcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BynowdotcomApplication.class, args);
+	}
+
+}
