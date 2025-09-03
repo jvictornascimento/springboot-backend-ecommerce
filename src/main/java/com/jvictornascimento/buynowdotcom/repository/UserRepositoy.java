0b293a4a65e22b1034476f609848e809a8e25812
jvictornascimento <victojoao789@gmail.com>
2025-09-02 22:40:11 -0300

    Implementing user security class

--- src/main/java/com/jvictornascimento/buynowdotcom/repository/UserRepositoy.java
+++ src/main/java/com/jvictornascimento/buynowdotcom/repository/UserRepositoy.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepositoy extends JpaRepository<User, Long> {
     boolean existsByEmail(String userEmail);
+
+    User findByEmail(String email);
 }
